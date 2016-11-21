1d326905a1c97c9dc7e908db1d305b7becebdf99
Xavier MARIN <marin.xavier@gmail.com>
2016-11-21 21:04:40 +0100

    wip

--- tp3/js/beers.js
+++ tp3/js/beers.js
@@ -2,11 +2,11 @@ $(document).ready(function(){
         $.get("js/data/beers.json", function (data) {
             var container = $('#beersList');
             data.forEach(function(item) {
-                var elem = $('<li></li>').addClass('list-group-item');
                 var alc = $('<span></span>').addClass('badge').text(item.alcohol);
-                elem.html(alc).append(' ' + item.name);
+                var elem = $('<li></li>').addClass('list-group-item').append(alc, item.name);
                 container.append(elem);
             });
-            $(".result").html(data);
+            var total = $('<p></p>').append('Total number of beers: ',data.length);
+            container.after(total);
         });
     });
